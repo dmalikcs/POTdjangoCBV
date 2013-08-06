dcd800124b4dc95257499e02bbed98096cefef9e
dmalikcs <dmalikcs@gmail.com>
2013-08-07 01:28:14 +0530

    RedirectView Tested

--- CBVTest/views.py
+++ CBVTest/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from .models import Publisher,Author,Book
+from django.views.generic.base import RedirectView
+
+
+class ThanksRedirectView(RedirectView):
+    def get_redirect_url(self):
+        return "/cbv/thanks/"
+
