4d31806030d2418da10f0e3f780abfa593736ba4
dmalikcs <dmalikcs@gmail.com>
2013-08-07 01:27:12 +0530

    TemplateView Tested

---
+++ CBVTest/urls.py
@@ -0,0 +1,27 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+from CBVTest.views import ThanksRedirectView
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^templateview/$', TemplateView.as_view(
+        template_name="CBVTest.html"
+        ),
+        ),
+    # url(r'^POTdjangoCBV/', include('POTdjangoCBV.foo.urls')),
+    url(r'redirect/$',ThanksRedirectView.as_view()),
+    url(r'thanks/$',TemplateView.as_view(
+        template_name='thanks.html'
+        ),
+        name='cbvthanks'),
+
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    )
